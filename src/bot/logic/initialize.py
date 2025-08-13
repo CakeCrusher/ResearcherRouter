dc0c4b6cc94f718c186e33e6ef260546bd795315
tiffh <tiffhuang321@gmail.com>
2025-08-13 09:32:36 -0400

    archived thread check on initializing

--- src/bot/logic/initialize.py
+++ src/bot/logic/initialize.py
@@ -1,7 +1,7 @@
 import os
 from .add_thread import *
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-from src.qdrant.qdrant import create_collection
+from src.qdrant.qdrant import create_collection, delete_thread_id
 # Redirect storage from Forum to store information to the vector database
 # Logs all items in the forum at startup
 async def initialize(bot):
@@ -9,22 +9,29 @@ async def initialize(bot):
     # Create the Qdrant collection on startup
     create_collection()
     channel_ID = int(os.getenv('PAPER_CHANNEL_ID')) # replace channelID with cool-papers channelID
+    channel_threads = 0
     sumID = int(os.getenv('SUMMARIZED_TAG'))
     logID = int(os.getenv('LOGGED_TAG'))
     paper_channel = bot.get_channel(channel_ID)
 
     # Command line statements
     print(f"Initializing forum channel: {paper_channel.name}")
-    print(f"Found {len(paper_channel.threads)} threads")
 
-    # For each Thread channel in the Forum (cool-papers)
+    '''Checks each active summarized thread in the forum'''
     for thread in paper_channel.threads:
-        
-        # We only need threads with the Summarized tag
-        # Checks if the thread is Summarized but not Logged
+        thread = await bot.fetch_channel(thread.id)
         tags = [tag.id for tag in thread.applied_tags]
         if (sumID in tags) and (logID not in tags):
             await add_thread(bot, thread)
+            channel_threads += 1
     
+    '''Checks each archived summarized thread in the forum (old posts 30> days)'''
+    async for thread in paper_channel.archived_threads(limit=None):
+        thread = await bot.fetch_channel(thread.id)
+        tags = [tag.id for tag in thread.applied_tags]
+        if (sumID in tags) and (logID not in tags):
+            await add_thread(bot, thread)
+            channel_threads += 1
+    print(f"Found {channel_threads} threads")
     print("Initialization complete!")
         
