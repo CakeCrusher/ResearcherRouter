1fedb62a0b62637f6d8d3a5e4df8b9fd61275403
tiffh <tiffhuang321@gmail.com>
2025-08-13 10:37:23 -0400

    thread owner and admin can manage thread summary

--- src/bot/logic/thread_summary.py
+++ src/bot/logic/thread_summary.py
@@ -1,5 +1,5 @@
 import discord
-
+import os
 '''
 Check whether the payload Emoji is the correct one.
 False: Summary = None
@@ -7,23 +7,40 @@ True: Summary = message.content
 
 NOTE: ENABLE MANAGE MESSAGES FOR BOT IN FORUM
 '''
-async def thread_summary(message, payload, channel):
+def can_manage_thread(payload, thread):
+    allowed_role_ids = [int(id) for id in os.getenv('ALLOWED_ROLE_IDS', '').split(',')]
+    member_roles = [role.id for role in payload.member.roles]
+    has_role = False
+    for role in allowed_role_ids:
+        if role in member_roles:
+            has_role = True
+ 
+    return (payload.member == thread.owner
+        or has_role
+        or payload.member.guild_permissions.administrator
+    )
+
+async def thread_summary(message, payload, thread):
 
     emoji = '📌'
     emoji_count = 0
     pin_reaction = discord.utils.get(message.reactions, emoji=emoji)
-    messages = [message async for message in channel.history(limit=None)]
+    messages = [message async for message in thread.history(limit=None)]
     this_reaction = payload.emoji
 
+    permission = can_manage_thread(payload, thread)     
 
     '''Removes extra 📌 emojis'''
     for msg in messages:
         for reaction in msg.reactions:
             if reaction.emoji == emoji:
+                if not permission:
+                    await message.remove_reaction(this_reaction, payload.member)
+                    print('Reaction not allowed: not message author or admin.')
                 emoji_count += 1
             if (emoji_count > 1) and (this_reaction.name == emoji):  
                 print('This channel already contains a summary!')
-                await message.remove_reaction(this_reaction, message.author)
+                await message.remove_reaction(this_reaction, payload.member)
                 return
 
     if not payload.emoji.name == emoji:    # If the reaction != emoji
